3472fb76df27d980dba44171eea51d3608670bab
Ibrahim <144321381+Ibrah111m@users.noreply.github.com>
2024-09-06 11:28:42 +0200

    La in Login rendrering i appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Login } from './components/Login/Index';
+import UserAtom from './components/Login/UserAtom';
 import Navbar from './components/navbarcomponent/Navbar'; 
 
 function App() {
   return (
     <>
       <Navbar />  {/* Endast Navbar-komponenten visas */}
+      <Login/>
     </>
   );
 }
