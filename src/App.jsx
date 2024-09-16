82be67e6d5d37e2c51a517ccc795640eb9514759
dilanmahmod <dilanmahmod96@gmail.com>
2024-09-16 22:42:24 +0200

    Skapat homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from './components/navbarcomponent/Navbar'; 
+import HomePage from './components/homepage/HomePage';
 
 function App() {
   return (
     <>
       <Navbar />  {/* Endast Navbar-komponenten visas */}
+      <HomePage />  {/* Hemsidan visas under Navbar */}
     </>
   );
 }
 
 export default App;
+
