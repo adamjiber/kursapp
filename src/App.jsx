faa39dc772c5b1f3f792a123dd4692925b2c236d
bendarte <osman.stockholm@gmail.com>
2024-09-17 11:53:40 +0200

    tog bort dubbleringarna från app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import Navbar from "./components/navbarcomponent/Navbar";
 import { Login } from "./components/Login/Index";
+
 import UserAtom from "./components/Login/UserAtom";
-import { Login } from './components/Login/Index';
-import UserAtom from './components/Login/UserAtom';
-import Navbar from './components/navbarcomponent/Navbar'; 
+
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <>
       <Navbar /> {/* Endast Navbar-komponenten visas */}
       <Login />
-      <Navbar />  {/* Endast Navbar-komponenten visas */}
-      <Login/>
       <UserAtom />
+      <Chat />
     </>
   );
 }
